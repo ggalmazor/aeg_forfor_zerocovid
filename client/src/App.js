e796111a51fc1bcaf4082f1d9512e40c5ea8903a
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2022-05-12 12:13:58 +0200

    Extekolanak para Oskar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,53 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 import HealthWidget from './components/HealthWidget';
+import {io} from "socket.io-client";
 
 function App() {
-    const [healthData, setHealthData] = useState({location: 'AEG', temperature: '27', humidity: '57%', co2: '780'});
-    return (
-        <div className="App">
-            <HealthWidget data={healthData}/> 
-        </div>
-    );
+  // const [location, setLocation] = useState('aeg');
+  // const [temperature, setTemperature] = useState(-1);
+  // const [humidity, setHumidity] = useState(-1);
+  // const [internalCo2, setInternalCo2] = useState(-1);
+  const [healthData, setHealthData] = useState({location: 'aeg', temperature: -1, humidity: -1, internal_co2: -1});
+
+  useEffect(() => {
+    const socket = io("ws://localhost:4000", {
+      reconnectionDelay: 1_000
+    });
+
+    socket.on('connected', () => {
+      console.log("Received: 'connected'");
+      socket.emit('subscribe', {location: 'aeg', field: 'temperature'});
+      socket.emit('subscribe', {location: 'aeg', field: 'humidity'});
+      socket.emit('start');
+    })
+
+    socket.on('data', data => {
+      console.log();
+      // if (data.field === 'temperature')
+      //   setTemperature(data.value);
+      // if (data.field === 'humidity')
+      //   setHumidity(data.value);
+      // if (data.field === 'internal_co2')
+      //   setInternalCo2(data.value);
+
+      const algo = {
+        location: healthData.location,
+        temperature: healthData.temperature,
+        humidity: healthData.humidity,
+        internal_co2: healthData.internal_co2
+      };
+      algo[data.field] = data.value
+      setHealthData(algo);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <HealthWidget data={healthData} />
+      {/*<HealthWidget data={healthData} location={location} temperature={temperature} humidity={humidity} internal_co2={internalCo2}/>*/}
+    </div>
+  );
 }
 
 export default App;
--- client/src/components/HealthWidget.js
+++ client/src/components/HealthWidget.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
 function HealthWidget(props) {
-    const {location, temperature, humidity, co2} = props.data;
+  const {location, temperature, humidity, internal_co2} = props.data;
   return (
     <div>
-        <h1>HealthWidget</h1>
-        <p>{location}</p>
-        <p>{temperature}</p>
-        <p>{humidity}</p>
-        <p>{co2}</p>
+      <h1>HealthWidget</h1>
+      <p>Location: {location}</p>
+      <p>Temperature: {temperature}</p>
+      <p>Humidity: {humidity}</p>
+      <p>Internal CO2: {internal_co2}</p>
+      {/*<p>Location: {location} - {props.location}</p>*/}
+      {/*<p>Temperature: {temperature} - {props.temperature}</p>*/}
+      {/*<p>Humidity: {humidity} - {props.humidity}</p>*/}
+      {/*<p>Internal CO2: {internal_co2} - {props.internal_co2}</p>*/}
     </div>
-    
   )
 }
 
