d1de7f51b64efade8c4382d7cab7c4681c755bd1
Oskar <oskar.calvo@aeg.eus>
2022-05-12 13:34:33 +0200

    [FIX] update object with useState Hook

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,7 @@ import HealthWidget from './components/HealthWidget';
 import {io} from "socket.io-client";
 
 function App() {
-  // const [location, setLocation] = useState('aeg');
-  // const [temperature, setTemperature] = useState(-1);
-  // const [humidity, setHumidity] = useState(-1);
-  // const [internalCo2, setInternalCo2] = useState(-1);
+
   const [healthData, setHealthData] = useState({location: 'aeg', temperature: -1, humidity: -1, internal_co2: -1});
 
   useEffect(() => {
@@ -19,33 +16,20 @@ function App() {
       console.log("Received: 'connected'");
       socket.emit('subscribe', {location: 'aeg', field: 'temperature'});
       socket.emit('subscribe', {location: 'aeg', field: 'humidity'});
+      socket.emit('subscribe', {location: 'aeg', field: 'internal_co2'});
       socket.emit('start');
     })
 
     socket.on('data', data => {
-      console.log();
-      // if (data.field === 'temperature')
-      //   setTemperature(data.value);
-      // if (data.field === 'humidity')
-      //   setHumidity(data.value);
-      // if (data.field === 'internal_co2')
-      //   setInternalCo2(data.value);
-
-      const algo = {
-        location: healthData.location,
-        temperature: healthData.temperature,
-        humidity: healthData.humidity,
-        internal_co2: healthData.internal_co2
-      };
-      algo[data.field] = data.value
-      setHealthData(algo);
+      setHealthData(prevState => {
+        return {...prevState, [data.field]: data.value}
+      });
     });
   }, []);
 
   return (
     <div className="App">
       <HealthWidget data={healthData} />
-      {/*<HealthWidget data={healthData} location={location} temperature={temperature} humidity={humidity} internal_co2={internalCo2}/>*/}
     </div>
   );
 }
